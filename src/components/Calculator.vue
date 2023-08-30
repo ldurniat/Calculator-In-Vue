69ec91e5c0ee0200dbd7a182689e284e20adb84a
ldurniat <ldurniat@gmail.com>
2023-08-30 18:36:07 +0200

    Add listners for buttons with operators and numbers

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -15,6 +15,10 @@ function sign() {
 function percent() {
 	current.value = `${parseFloat(current.value)/100}`
 }
+
+function append(number) {
+	current.value = `${current.value}${number}`
+}
 </script>
 
 <template>
@@ -25,20 +29,20 @@ function percent() {
 		<div @click="clear" class="btn">C</div>
 		<div @click="sign" class="btn operator">+/-</div>
 		<div @click="percent" class="btn operator">%</div>
-		<div class="btn operator">/</div>
-		<div class="btn">7</div>
-		<div class="btn">8</div>
-		<div class="btn">9</div>
-		<div class="btn operator">*</div>
-		<div class="btn">4</div>
-		<div class="btn">5</div>
-		<div class="btn">6</div>
-		<div class="btn operator">-</div>
-		<div class="btn">1</div>
-		<div class="btn">2</div>
-		<div class="btn">3</div>
-		<div class="btn operator">+</div>
-		<div class="btn zero">0</div>
+		<div @click="append('/')" class="btn operator">/</div>
+		<div @click="append('7')" class="btn">7</div>
+		<div @click="append('8')" class="btn">8</div>
+		<div @click="append('9')" class="btn">9</div>
+		<div @click="append('*')" class="btn operator">*</div>
+		<div @click="append('4')" class="btn">4</div>
+		<div @click="append('5')" class="btn">5</div>
+		<div @click="append('6')" class="btn">6</div>
+		<div @click="append('-')" class="btn operator">-</div>
+		<div @click="append('1')" class="btn">1</div>
+		<div @click="append('2')" class="btn">2</div>
+		<div @click="append('3')" class="btn">3</div>
+		<div @click="append('+')" class="btn operator">+</div>
+		<div @click="append('0')" class="btn zero">0</div>
 		<div class="btn">.</div>
 		<div class="btn operator">=</div>
 		<div class="btn"></div>
