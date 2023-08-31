9fdd058800a6207982f6cc99a9efd1c679c02ae7
ldurniat <ldurniat@gmail.com>
2023-08-31 21:52:13 +0200

    Add additional styles

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@ import Calculator from './components/Calculator.vue'
 	<Calculator />
 </template>
 
-<style scoped>
-
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
